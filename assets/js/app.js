820a1e510b30671b79de739771b173376b588916
leebohyeong <qhgud9371@gmail.com>
2025-04-22 17:53:53 +0900

    js 작업중

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
-// header
+// Header Menu
+{
+  const siteMenuContent = document.querySelector(".site-menu__content");
+  const toggleMenu = document.querySelector(".site-header__toggle");
+  const toggleMenuItems = siteMenuContent.querySelectorAll(".site-menu__link");
+
+  toggleMenu.addEventListener("click", () => {
+    siteMenuContent.classList.toggle("site-menu__content--open");
+  });
+
+  //   siteMenuContent.addEventListener("click", () => {
+  //     siteMenuContent.classList.remove("site-menu__content--open");
+  //   });
+  toggleMenuItems.forEach((toggleMenuItem) => {
+    toggleMenuItem.addEventListener("click", () => {
+      siteMenuContent.classList.remove("site-menu__content--open");
+    });
+  });
+}
+
+// Header
 {
   const siteHeader = document.querySelector(".site-header");
   const siteHeaderHeight = siteHeader.offsetHeight;
@@ -12,12 +32,27 @@
   });
 }
 
-// home
+// Home
 {
   const homeContainer = document.querySelector(".home__container");
   const homeContainerHeight = homeContainer.offsetHeight;
 
   document.addEventListener("scroll", () => {
     homeContainer.style.opacity = 1 - window.scrollY / homeContainerHeight;
+  });
+}
+
+// Arrow up
+{
+  const home = document.querySelector(".home");
+  const homeHeight = home.offsetHeight;
+  const arrowUp = document.querySelector(".arrow-up__button");
+
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > homeHeight / 2) {
+      arrowUp.classList.add("arrow-up__button--active");
+    } else {
+      arrowUp.classList.remove("arrow-up__button--active");
+    }
   });
 }
