51bdaca051b59fad22f5d4aa7325b5321fbe0bc2
leebohyeong <qhgud9371@gmail.com>
2025-04-24 03:10:39 +0900

    js-observe 작업중

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,50 @@
   });
 }
 
+// Active Menu
+{
+  const sectionIds = [
+    "#home",
+    "#about",
+    "#skills",
+    "#work",
+    "#testimonials",
+    "#contact",
+  ];
+
+  const sections = sectionIds.map((id) => document.querySelector(id));
+  const navItems = sectionIds.map((id) =>
+    document.querySelector(`[href="${id}"]`)
+  );
+
+  const visibleSections = sectionIds.map(() => false);
+
+  const options = {};
+  const observer = new IntersectionObserver(observerCallback, options);
+  sections.forEach((section) => observer.observe(section));
+
+  function observerCallback(entries) {
+    let selectLastOne;
+    entries.forEach((entry) => {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      visibleSections[index] = entry.isIntersecting;
+      selectLastOne =
+        index === sectionIds.length - 1 &&
+        entry.isIntersecting &&
+        entry.intersectionRatio >= 0.99;
+    });
+
+    const navIndex = selectLastOne
+      ? sectionIds.length - 1
+      : findFirstIntersection(visibleSections);
+  }
+
+  function findFirstIntersection(intersections) {
+    const index = intersections.indexOf(true);
+    return index >= 0 ? index : 0;
+  }
+}
+
 // Header
 {
   const siteHeader = document.querySelector(".site-header");
