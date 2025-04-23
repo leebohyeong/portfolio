5d3404ecd10fe341fe477e6ec77ce207916ff2ff
leebohyeong <qhgud9371@gmail.com>
2025-04-23 18:18:01 +0900

    js-work 작업

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,6 @@
     siteMenuContent.classList.toggle("site-menu__content--open");
   });
 
-  //   siteMenuContent.addEventListener("click", () => {
-  //     siteMenuContent.classList.remove("site-menu__content--open");
-  //   });
   toggleMenuItems.forEach((toggleMenuItem) => {
     toggleMenuItem.addEventListener("click", () => {
       siteMenuContent.classList.remove("site-menu__content--open");
@@ -32,16 +29,6 @@
   });
 }
 
-// Home
-{
-  const homeContainer = document.querySelector(".home__container");
-  const homeContainerHeight = homeContainer.offsetHeight;
-
-  document.addEventListener("scroll", () => {
-    homeContainer.style.opacity = 1 - window.scrollY / homeContainerHeight;
-  });
-}
-
 // Arrow up
 {
   const home = document.querySelector(".home");
@@ -55,4 +42,54 @@
       arrowUp.classList.remove("arrow-up__button--active");
     }
   });
+}
+
+// Home
+{
+  const homeContainer = document.querySelector(".home__container");
+  const homeContainerHeight = homeContainer.offsetHeight;
+
+  document.addEventListener("scroll", () => {
+    homeContainer.style.opacity = 1 - window.scrollY / homeContainerHeight;
+  });
+}
+
+// Work
+{
+  const categoryButtons = document.querySelectorAll(".work-category__item");
+  const projectContainer = document.querySelector(".work-projects");
+  const projectItems = document.querySelectorAll(".work-projects__item");
+
+  categoryButtons.forEach((categoryButton) => {
+    categoryButton.addEventListener("click", (event) => {
+      const filter = categoryButton.dataset.category;
+
+      handleActiveSelection(event.target);
+      filterProjects(filter);
+    });
+  });
+
+  const handleActiveSelection = (target) => {
+    const activeButton = document.querySelector(
+      ".work-category__item--selected"
+    );
+    activeButton.classList.remove("work-category__item--selected");
+    target.classList.add("work-category__item--selected");
+  };
+
+  const filterProjects = (filter) => {
+    projectItems.forEach((projectItem) => {
+      if (filter === "All" || filter === projectItem.dataset.type) {
+        projectItem.style.display = "block";
+      } else {
+        projectItem.style.display = "none";
+      }
+    });
+
+    projectContainer.classList.add("animation-out");
+
+    setTimeout(() => {
+      projectContainer.classList.remove("animation-out");
+    }, 250);
+  };
 }
