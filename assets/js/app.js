d43206cb869bf10fbeb1882689e06fa8d33b0896
leebohyeong <qhgud9371@gmail.com>
2025-04-24 04:01:54 +0900

    js-observer 작업

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,10 +30,13 @@
   const navItems = sectionIds.map((id) =>
     document.querySelector(`[href="${id}"]`)
   );
-
   const visibleSections = sectionIds.map(() => false);
+  let activeNavItem = navItems[0];
 
-  const options = {};
+  const options = {
+    rootMargin: "-20% 0px 0px 0px",
+    threshold: [0, 0.98],
+  };
   const observer = new IntersectionObserver(observerCallback, options);
   sections.forEach((section) => observer.observe(section));
 
@@ -45,17 +48,27 @@
       selectLastOne =
         index === sectionIds.length - 1 &&
         entry.isIntersecting &&
-        entry.intersectionRatio >= 0.99;
+        entry.intersectionRatio >= 0.95;
     });
 
     const navIndex = selectLastOne
       ? sectionIds.length - 1
-      : findFirstIntersection(visibleSections);
+      : findFirstIntersecting(visibleSections);
+
+    selectNavItem(navIndex);
   }
 
-  function findFirstIntersection(intersections) {
+  function findFirstIntersecting(intersections) {
     const index = intersections.indexOf(true);
     return index >= 0 ? index : 0;
+  }
+
+  function selectNavItem(index) {
+    const navItem = navItems[index];
+    if (!navItem) return;
+    activeNavItem.classList.remove("site-menu__link--active");
+    activeNavItem = navItem;
+    activeNavItem.classList.add("site-menu__link--active");
   }
 }
 
